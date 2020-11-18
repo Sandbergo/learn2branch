e8406dda9d5707b3e0c8f10181db2d7ed19663fd
Sandbergo <larslsa@stud.ntnu.no>
2020-11-18 15:12:44 +0100

    added barplot

--- analyze_training.py
+++ analyze_training.py
@@ -11,9 +11,9 @@ def plot_loss(str_log):
     val_regex = re.compile('VALID LOSS: [0-9.]{5}')
     val_loss = val_regex.findall(str_log)
     val_loss = [float(x[-5:]) for x in val_loss]
-    
+
     global_steps = list(range(0, len(train_loss)*312, 312))
-   
+
     plt.figure(figsize=(8, 4))
     plt.ylim([2.0, 4.0])
     plt.plot(global_steps, train_loss, "-", label="Training Loss")
@@ -40,8 +40,27 @@ def plot_kacc(str_log):
     plt.savefig("figures/test2.png")
 
 
+def barplot_kacc(str_log):
+    regex = re.compile('@[0-9]{1,2}: [0-9.]{5}')
+    acc = regex.findall(str_log)
+    acc = [float(x[-5:]) for x in acc]
+
+    import seaborn as sns
+    import pandas as pd
+    sns.set_theme(style="whitegrid")
+    acc_df = pd.DataFrame(
+        {'acc@1': [acc[-4]],
+         'acc@3': [acc[-3]],
+         'acc@5': [acc[-2]],
+         'acc@10': [acc[-1]]}
+        )
+    ax = sns.barplot(data=acc_df, ci=None)
+    ax.figure.savefig("figures/barplot.png")
+
+
 if __name__ == "__main__":
     with open('trained_models/cauctions/mlp_sigmoidal_decay/0/log.txt') as log:
         str_log = log.read()
         plot_loss(str_log)
         plot_kacc(str_log)
+        barplot_kacc(str_log)
